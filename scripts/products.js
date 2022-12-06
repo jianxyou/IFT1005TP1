66dd8c6e61c5c6cdadc1bc3e4fa706936b3767b0
Cynthia <cynthiaphan98@gmail.com>
2022-12-05 21:44:08 -0500

    Add request to products.json with AJAX

--- scripts/products.js
+++ scripts/products.js
@@ -1,2 +1,29 @@
 // Jianxin You 20134401
 // Hoang-Thi-Thi Cynthia Phan 20220019
+
+const main = document.querySelector('.products--items');
+const url = './data/products.json';
+
+onload = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+        const data = JSON.parse(xhr.responseText);
+        loadProducts(data);
+    }
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+function loadProducts(products) {
+    let html = ''; 
+    products.forEach(product => {
+        html += `<a class="items" href="./product.html?id=${product.id}">
+            <article>
+                <h3>${product.name}</h3>
+                <img src="./imgs/${product.image}" alt="${product.name}">
+                <p>${product.price}</p>
+            </article>
+        </a>`
+    });
+    main.innerHTML = html;
+}
